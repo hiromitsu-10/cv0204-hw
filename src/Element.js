85ed58c27f1ae9ab4990683aaf12238acb568a37
kurokawa <tkuro1026@icloud.com>
2020-02-07 14:40:12 +0900

    変更しました

---
+++ src/Element.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Element = () => {
+    return (
+        <h1>こんにちは</h1>
+    )
+}
+
+export default Element
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 拡張子はいらない
+// import Element from "./Element"
+import App from "./App"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(<h1>Hello world</h1>,
+//     document.getElementById("root")
+// )
+
+// const Element = () => {
+//     return (
+//         <h1>こんにちは</h1>
+//     )
+// }
+
+// const App = () => {
+//     return (
+//         <>
+//             <Element />
+//             <Element />
+//         </>
+//     )
+// }
+
+ReactDOM.render(<App />,
+    document.getElementById("root")
+)
